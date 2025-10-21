451f778e5839860c90d944bada09453c68450f96
EmiliaCabrer1 <emi.22692@gmail.com>
2025-10-20 21:32:17 -0300

    Volver

--- src/Componentes/Encabezado.jsx
+++ src/Componentes/Encabezado.jsx
@@ -13,67 +13,65 @@ const Encabezado = () => {
 
   return (
     <>
-      <nav className="flex fixed top-0">
-        <div className="relative">
-          <div className="sm:hidden w-full">
-            {!expanded && (
-              <>
+      <nav className="flex relative">
+        <div className="sm:hidden w-full">
+          {!expanded && (
+            <>
+              <img
+                className="w-full h-auto"
+                src="/img/navCerrada.svg"
+                alt="Navbar"
+              />
+              <button
+                className={"absolute bottom-0 right-[2vw]"}
+                onClick={() => setExpanded(!expanded)}
+              >
                 <img
-                  className="w-full h-auto"
-                  src="/img/navCerrada.svg"
-                  alt="Navbar"
+                  src="/img/hamburgesa.svg"
+                  alt="Menu Hamburguesa"
+                  className="object-fill w-[20vw] h-auto"
                 />
+              </button>
+            </>
+          )}
+          {expanded && (
+            <>
+              <img
+                className="w-full h-auto"
+                src="/img/navAbierta.svg"
+                alt="Navbar"
+              />
+              <div className="w-[40vw] h-auto z-20 bg-verde-intermedio shadow-custom-shadow rounded-2xl absolute top-[70%] right-[2vw] p-3">
+                <ul className="text-white px-3 mx-auto">
+                  {secciones.map((seccion, index) => (
+                    <li
+                      key={index}
+                      className={
+                        index < secciones.length - 1
+                          ? "border-b-1 mb-2"
+                          : "mb-2"
+                      }
+                    >
+                      <a href={seccion.link}>{seccion.nombre}</a>
+                    </li>
+                  ))}
+                </ul>
                 <button
-                  className={"absolute bottom-0 right-[2vw]"}
+                  className="absolute top-1 right-4 text-white"
                   onClick={() => setExpanded(!expanded)}
                 >
-                  <img
-                    src="/img/hamburgesa.svg"
-                    alt="Menu Hamburguesa"
-                    className="object-fill w-[20vw] h-auto"
-                  />
+                  x
                 </button>
-              </>
-            )}
-            {expanded && (
-              <>
-                <img
-                  className="w-full h-auto"
-                  src="/img/navAbierta.svg"
-                  alt="Navbar"
-                />
-                <div className="w-[40vw] h-auto z-20 bg-verde-intermedio shadow-custom-shadow rounded-2xl absolute top-[70%] right-[2vw] p-3">
-                  <ul className="text-white px-3 mx-auto">
-                    {secciones.map((seccion, index) => (
-                      <li
-                        key={index}
-                        className={
-                          index < secciones.length - 1
-                            ? "border-b-1 mb-2"
-                            : "mb-2"
-                        }
-                      >
-                        <a href={seccion.link}>{seccion.nombre}</a>
-                      </li>
-                    ))}
-                  </ul>
-                  <button
-                    className="absolute top-1 right-4 text-white"
-                    onClick={() => setExpanded(!expanded)}
-                  >
-                    x
-                  </button>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="hidden sm:flex w-full h-auto bg-verde-intermedio shadow-custom-shadow justify-end pr-8">
-            {secciones.map((seccion, index) => (
-              <h1 key={index} className="text-white text-lg px-2 my-4">
-                <a href={seccion.link}>{seccion.nombre}</a>
-              </h1>
-            ))}
-          </div>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="hidden sm:flex w-full h-auto bg-verde-intermedio shadow-custom-shadow justify-end pr-8">
+          {secciones.map((seccion, index) => (
+            <h1 key={index} className="text-white text-lg px-2 my-4">
+              <a href={seccion.link}>{seccion.nombre}</a>
+            </h1>
+          ))}
         </div>
       </nav>
     </>
