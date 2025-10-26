3650c4eb483791eb803060113345be6b9b99a510
Cai <tommasiricardo@gmail.com>
2025-10-25 21:06:23 -0300

    80% en juego portada.

--- src/Paginas/JuegoPortada.jsx
+++ src/Paginas/JuegoPortada.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const JuegoPortada = () => {
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="flex relative mx-auto items-center h-[70vh] w-[85vw] rounded-2xl gap-3 border-verde-intermedio border-1 px-4 py-12 justify-around overflow-visible">
+      <div className="flex relative mx-auto items-center h-[80%] w-[85vw] rounded-2xl gap-3 border-verde-intermedio border-1 px-4 py-12 justify-around overflow-visible">
         <div className="flex overflow-visible -mr-7 items-center justify-center ">
           <a href="/datacartas">
             <img src="/img/cartasPortada.svg" alt="cartas img" />
