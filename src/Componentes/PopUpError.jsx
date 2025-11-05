d9fde8ac724cf1001b0e575502a462ebf4fdb6a4
Emilia <emi.22692@gmail.com>
2025-11-05 15:56:16 -0300

    correccion medidas popup pc

--- src/Componentes/PopUpError.jsx
+++ src/Componentes/PopUpError.jsx
@@ -1,10 +1,10 @@
 const PopUpError = ({ mensaje, onClick }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[rgba(95,111,102,0.7)] z-80 w-[100vw] h-[100vh]">
-      <div className="flex flex-col bg-fondo-claro rounded-lg shadow-xl overflow-hidden w-[80vw] sm:[50vw] h-[20vh] sm:h-[40vh] p-10">
+      <div className="flex flex-col justify-center items-center bg-fondo-claro rounded-lg shadow-xl overflow-hidden w-[80vw] sm:w-[40vw] h-[20vh] sm:h-[20vh] p-10">
         <h6 className="text-[#36463E]">{mensaje}</h6>
         <button
-          className="text-white text-xs font-medium bg-[#36463E] px-4 py-2 rounded-lg shadow-lg active:shadow-inner cursor-pointer w-auto mt-4"
+          className="text-white text-xs font-medium bg-[#36463E] px-4 py-2 rounded-lg shadow-lg active:shadow-inner cursor-pointer sm:w-30 mt-4"
           onClick={onClick}
         >
           Aceptar
